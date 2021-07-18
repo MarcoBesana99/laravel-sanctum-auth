350a221bd9fc60e98b879c98b09638a99499754c
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-07-18 16:54:40 +0200

    logout route and function

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout']);
